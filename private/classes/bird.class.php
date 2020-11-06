c8fb428f4e1c46722951b8ea988274980cc4a616
Miles <mileshollifieldgfp@gmail.com>
2020-11-05 23:20:21 -0500

    Finish asgn08-rud

--- private/classes/bird.class.php
+++ private/classes/bird.class.php
@@ -1,15 +1,13 @@
 <?php
 
  class Bird {
-
-    // -- Start of Active Record Code -- //
     
     static protected $database;
     static protected $db_columns = ['id', 'common_name', 'habitat', 'food', 'conservation_id', 'backyard_tips'];
     public $errors = [];
    
     static public function set_database($database) {
-        self::$database = $database;
+      self::$database = $database;
     }
 
     static public function find_by_sql($sql) {
@@ -30,7 +28,7 @@
     static public function find_by_id($id) {
         $sql = "SELECT * FROM birds ";
         $sql .= "WHERE id=" . self::$database->quote($id);
-        $object_array = self::find_by_sql($sql);
+        $object_array = static::find_by_sql($sql);
         if(!empty($object_array)) {
             return array_shift($object_array);
         }   else    {
@@ -39,7 +37,7 @@
     }
     static public function find_all() {
         $sql = "SELECT * FROM birds";
-        return self::find_by_sql($sql);
+        return static::find_by_sql($sql);
     }
 
     static public function instantiate($record) {
@@ -52,37 +50,6 @@
         return $object;
     }
 
-
-
-    // -- End of Active Record Code -- //
-
-    public $id;
-    public $common_name;
-    public $habitat;
-    public $food;
-    public $nest_palcement;
-    public $behavior;
-    public $backyard_tips;
-    public $conservation_id=1;
-
-    public const CONSERVATION_OPTIONS = [ 
-        1 => "Low concern",
-        2 => "Medium concern",
-        3 => "High concern",
-        4 => "Extreme concern"
-    ];
-
-    public function __construct($args=[]) {
-        $this->common_name = $args['common_name'] ?? '';
-        $this->habitat = $args['habitat'] ?? '';
-        $this->food = $args['food'] ?? '';
-        $this->nest_palcement = $args['nest_palcement'] ?? '';
-        $this->behavior = $args['behavior'] ?? '';
-        $this->backyard_tips = $args['backyard_tips'] ?? '';
-        $this->conservation_id = $args['conservation_id'] ?? '';
-
-    }
-    
     public function create_not_bound() {
         $sql = "INSERT INTO birds (common_name, habitat, food, conservation_id, backyard_tips)";
         $sql .= " VALUES (";
@@ -143,7 +110,6 @@
         $stmt->bindValue(':conservation_id', $this->conservation_id );
         $stmt->bindValue('backyard_tips', $this->backyard_tips );
         
-        //$result = self::$database->exec($sql);
         $result = $stmt->execute();
 
         if( $result ) {
@@ -205,7 +171,7 @@
     // Properties which have database columns, excluding ID
     public function attributes() {
       $attributes = [];
-      foreach(self::$db_columns as $column) {
+      foreach(static::$db_columns as $column) {
         if ($column == 'id') { continue; }
         $attributes[$column] = $this->$column;
       }
@@ -234,6 +200,33 @@
       // but, for example, we can't call $user->update() after calling $user->delete().
     }
    
+    public $id;
+    public $common_name;
+    public $habitat;
+    public $food;
+    public $nest_palcement;
+    public $behavior;
+    public $backyard_tips;
+    public $conservation_id=1;
+
+    public const CONSERVATION_OPTIONS = [ 
+        1 => "Low concern",
+        2 => "Medium concern",
+        3 => "High concern",
+        4 => "Extreme concern"
+    ];
+
+    public function __construct($args=[]) {
+        $this->common_name = $args['common_name'] ?? '';
+        $this->habitat = $args['habitat'] ?? '';
+        $this->food = $args['food'] ?? '';
+        $this->nest_palcement = $args['nest_palcement'] ?? '';
+        $this->behavior = $args['behavior'] ?? '';
+        $this->backyard_tips = $args['backyard_tips'] ?? '';
+        $this->conservation_id = $args['conservation_id'] ?? '';
+
+    }
+    
     public function conservation() {
         // echo self::CONSERVATION_OPTIONS[$this->conservation_id];
         if( $this->conservation_id > 0 ) {
@@ -243,5 +236,5 @@
         }
     }
 
-
-}
+ }
+?>
--- private/initialize.php
+++ private/initialize.php
@@ -32,12 +32,17 @@
   include('classes/parsecsv.class.php');
   // Autoload class definitions
 
-  function my_autoload($class) {
-    if ( preg_match('/\A\w+\Z/', $class) ) {
-      include 'classes/' . $class . 'class.php';
-    }
+  // -> All classes in directory
+  foreach(glob('classes/*.class.php') as $file) {
+    require_once($file);
   }
 
+  // Autoload class definitions
+  function my_autoload($class) {
+    if(preg_match('/\A\w+\Z/', $class)) {
+      include('classes/' . $class . '.class.php');
+    }
+  }
   spl_autoload_register('my_autoload');
 
   $database = db_connect();
